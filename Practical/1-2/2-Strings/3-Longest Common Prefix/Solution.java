cc38e89eda1122abc9c2f70765d084a09d9468c7
BenoDavid <beno.david@classicfashion.com>
2025-03-20 16:48:20 +0300

    fix

--- Practical/1-2/2-Strings/3-Longest Common Prefix/Solution.java
+++ Practical/1-2/2-Strings/3-Longest Common Prefix/Solution.java
@@ -1,4 +1,22 @@
+public class Solution {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
 
-class Solution {
+        String prefix = strs[0]; // Start with the first string
 
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1); // Reduce prefix
+                if (prefix.isEmpty())
+                    return "";
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestCommonPrefix(new String[] { "flower", "flow", "flight" })); // Output: "fl"
+        System.out.println(longestCommonPrefix(new String[] { "dog", "racecar", "car" })); // Output: ""
+    }
 }

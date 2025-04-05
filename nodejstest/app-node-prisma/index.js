566913226acc8726c38ef19016e96003888570c0
BenoDavid <beno.david@classicfashion.com>
2025-04-05 16:31:49 +0300

    f

--- nodejstest/app-node-prisma/index.js
+++ nodejstest/app-node-prisma/index.js
@@ -2,20 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const { PrismaClient } = require("@prisma/client");
-
-const prisma = new PrismaClient();
+const routes = require('./routes');
 
 app.use(express.json());
 
 // Routes will be imported later
-const userRoutes = require("./routes/user.routes");
-const productRoutes = require("./routes/product.routes");
-const orderRoutes = require("./routes/order.routes");
+app.use('/api/', routes);
 
-app.use("/api/users", userRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/orders", orderRoutes);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);

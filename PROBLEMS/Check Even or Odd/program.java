58c3e3966c1bb831bc533c14310102dccf9fdddc
BenoDavid <beno.david@classicfashion.com>
2025-10-04 17:58:02 +0300

    fix

--- PROBLEMS/Check Even or Odd/program.java
+++ PROBLEMS/Check Even or Odd/program.java
@@ -1,7 +1,15 @@
 import java.util.*;
 
 public class program {
+    public static boolean isEven(int num){
+        return num % 2 == 0;
+    }
      public static void main(String[] args){
-         
-     }
+         int n= 13;
+         if(isEven(n)){
+            System.out.println("even");
+         }else{
+            System.out.println("odd");
+         }
+     }  
 }
